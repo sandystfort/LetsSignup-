89fffedd5e609b00772d151810709dfb7dfc1714
sandystfort <148255580+sandystfort@users.noreply.github.com>
2024-11-15 00:31:12 -0500

    added the logo

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,17 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import getUserInfo from "../utilities/decodeJwt";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import ReactNavbar from "react-bootstrap/Navbar";
-import "./pages/navbar.css";
+import "./pages/navbar.css"; // Ensure the CSS path is correct
 
+// Here, we display our Navbar
 export default function Navbar() {
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useState({});
+
   useEffect(() => {
-    getUserInfo(); // You can still call getUserInfo() if needed for side effects
+    setUser(getUserInfo());
   }, []);
 
   return (
     <ReactNavbar className="custom-navbar">
+      {" "}
+      {/* Apply custom class */}
       <Container>
         <Nav className="me-auto">
           <Nav.Link href="/">Start</Nav.Link>
--- frontend/src/components/pages/landingPage.js
+++ frontend/src/components/pages/landingPage.js
@@ -2,7 +2,9 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-import SSULogo from "../../SSU.png"; // Adjust the path to match your project structure
+import SSULogo from "../SSU.png";
+
+// Adjust the path to match your project structure
 import "./landingPage.css"; // Ensure to link the CSS file
 
 const LandingPage = () => {
