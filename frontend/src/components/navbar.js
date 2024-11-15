c16b75dc75e6c1c70c92b62ef984ffdc7af2a7b3
sandystfort <148255580+sandystfort@users.noreply.github.com>
2024-11-14 23:24:20 -0500

    // changing colors

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,25 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import getUserInfo from "../utilities/decodeJwt";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import ReactNavbar from "react-bootstrap/Navbar";
+import "./pages/navbar.css";
 
-// Here, we display our Navbar
 export default function Navbar() {
-  // We are pulling in the user's info but not using it for now.
-  // Warning disabled:
-  // eslint-disable-next-line
-  const [user, setUser] = useState({});
-
   useEffect(() => {
-    setUser(getUserInfo());
+    getUserInfo(); // You can still call getUserInfo() if needed for side effects
   }, []);
 
-  // if (!user) return null   - for now, let's show the bar even not logged in.
-  // we have an issue with getUserInfo() returning null after a few minutes
-  // it seems.
   return (
-    <ReactNavbar bg="dark" variant="dark">
+    <ReactNavbar className="custom-navbar">
       <Container>
         <Nav className="me-auto">
           <Nav.Link href="/">Start</Nav.Link>
